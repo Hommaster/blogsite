2c5b9a7671c0d4ed3c310081aaead51910af421a
Hommaster <127387220+Hommaster@users.noreply.github.com>
2023-09-03 19:47:39 +0300

    Update settings.py

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -134,6 +134,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Конфигурация сервера электронной почты
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'ilya.pan.2017@gmail.com'
-EMAIL_HOST_PASSWORD ='efrgxuwresqfkdrx'
-EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD =''
+EMAIL_PORT = 1
 EMAIL_USE_TLS = True
